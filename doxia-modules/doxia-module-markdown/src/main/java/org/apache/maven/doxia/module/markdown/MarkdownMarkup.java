ae9f49f7c71210e7342c364b706340fbc6c20f7c
Mariano A. Cortesi <mariano@zauberlabs.com>
2011-12-28 17:24:17 -0300

    work in progress. Sink Test

---
+++ doxia-modules/doxia-module-markdown/src/main/java/org/apache/maven/doxia/module/markdown/MarkdownMarkup.java
@@ -0,0 +1,120 @@
+package org.apache.maven.doxia.module.markdown;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.swing.text.html.HTML.Tag;
+
+import org.apache.maven.doxia.markup.TextMarkup;
+
+/**
+ * This interface defines all markups and syntaxes used by the <b>Markdown</b> format.
+ *
+ * @see <a href="http://daringfireball.net/projects/markdown/syntax">http://daringfireball.net/projects/markdown/syntax</a>
+ * @author <a href="mailto:mariano@zauberlabs.com">Mariano Cortesi</a>
+ * @version $Id$
+ * @since 1.3
+ */
+public interface MarkdownMarkup
+    extends TextMarkup
+{
+    // ----------------------------------------------------------------------
+    // Markdown markups
+    // ----------------------------------------------------------------------
+
+    /** Syntax for the anchor : '#' */
+    char ANCHOR_MARKUP = '#';
+
+    /** Syntax for the start line separator: "   " */
+    String THREE_SPACES_MARKUP = "   ";
+
+    /** Syntax for the bold markup: "**" */
+    String BOLD_END_MARKUP = "**";
+
+    /** Syntax for the bold markup: "**" */
+    String BOLD_START_MARKUP = "**";
+
+    /** Syntax for the bold italic markup: "____" */
+    String BOLD_ITALIC_END_MARKUP = "___";
+
+    /** Syntax for the bold italic markup: "___" */
+    String BOLD_ITALIC_START_MARKUP = "___";
+
+    /** Syntax for the bold monospaced markup: "==" */
+    String BOLD_MONOSPACED_END_MARKUP = "==";
+
+    /** Syntax for the bold monospaced markup: "==" */
+    String BOLD_MONOSPACED_START_MARKUP = "==";
+
+    /** Syntax for the horizontal rule markup: "---" */
+    String HORIZONTAL_RULE_MARKUP = "---";
+
+    /** Syntax for the italic markup: "_" */
+    String ITALIC_END_MARKUP = "_";
+
+    /** Syntax for the italic markup: "_" */
+    String ITALIC_START_MARKUP = "_";
+
+    /** Syntax for the link end markup: "]]" */
+    String LINK_END_MARKUP = "]]";
+
+    /** Syntax for the link middle markup: "][" */
+    String LINK_MIDDLE_MARKUP = "][";
+
+    /** Syntax for the link start markup: "[[" */
+    String LINK_START_MARKUP = "[[";
+
+    /** Syntax for the list item markup: "*" */
+    String LIST_ITEM_MARKUP = "* ";
+
+    /** Syntax for the mono-spaced style end: "=" */
+    String MONOSPACED_END_MARKUP = "`";
+
+    /** Syntax for the mono-spaced style start: "=" */
+    String MONOSPACED_START_MARKUP = "`";
+
+    /** Syntax for the numbering decimal markup char: "1." */
+    String NUMBERING_MARKUP = "1.";
+
+    /** Syntax for the numbering lower alpha markup char: "a." */
+    String NUMBERING_LOWER_ALPHA_MARKUP = "a.";
+
+    /** Syntax for the numbering lower roman markup char: "i." */
+    String NUMBERING_LOWER_ROMAN_MARKUP = "i.";
+
+    /** Syntax for the numbering upper alpha markup char: "A." */
+    String NUMBERING_UPPER_ALPHA_MARKUP = "A.";
+
+    /** Syntax for the numbering upper roman markup char: "I." */
+    String NUMBERING_UPPER_ROMAN_MARKUP = "I.";
+
+    // ----------------------------------------------------------------------
+    // Specific Markdown tags
+    // ----------------------------------------------------------------------
+
+    /** TWiki tag for <code>verbatim</code> */
+    Tag VERBATIM_TAG = new Tag()
+    {
+        /** {@inheritDoc} */
+        public String toString()
+        {
+            return "verbatim";
+        }
+    };
+}
--- doxia-modules/doxia-module-markdown/src/test/java/org/apache/maven/doxia/module/markdown/MarkdownSinkTest.java
+++ doxia-modules/doxia-module-markdown/src/test/java/org/apache/maven/doxia/module/markdown/MarkdownSinkTest.java
@@ -1,252 +1,288 @@
 package org.apache.maven.doxia.module.markdown;
 
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.BOLD_ITALIC_END_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.BOLD_ITALIC_START_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.HORIZONTAL_RULE_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.ITALIC_END_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.ITALIC_START_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.LIST_ITEM_MARKUP;
+import static org.apache.maven.doxia.module.markdown.MarkdownMarkup.NUMBERING_LOWER_ROMAN_MARKUP;
+
 import java.io.Writer;
 
 import org.apache.maven.doxia.sink.AbstractSinkTest;
 import org.apache.maven.doxia.sink.Sink;
-
-/*
- * Copyright (c) 2011 Zauber S.A. -- All rights reserved
- */
+import org.codehaus.plexus.util.StringUtils;
 
 /**
  * Tests the Sink's Functionality
- * 
  * 
  * @author Cristian Pereyra
  * @since Dec 26, 2011
  */
 public class MarkdownSinkTest extends AbstractSinkTest{
 
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#createSink(java.io.Writer) */
+    /** {@inheritDoc} */
     @Override
-    protected Sink createSink(Writer arg0) {
-        // TODO: Auto-generated method stub
-        return null;
+    protected Sink createSink( Writer writer) {
+        return new MarkdownSink(writer);
     }
 
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getAnchorBlock(java.lang.String) */
-    @Override
-    protected String getAnchorBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getAuthorBlock(java.lang.String) */
-    @Override
-    protected String getAuthorBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getBodyBlock() */
-    @Override
-    protected String getBodyBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getBoldBlock(java.lang.String) */
-    @Override
-    protected String getBoldBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getCommentBlock(java.lang.String) */
-    @Override
-    protected String getCommentBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getDateBlock(java.lang.String) */
-    @Override
-    protected String getDateBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getDefinitionListBlock(java.lang.String, java.lang.String) */
-    @Override
-    protected String getDefinitionListBlock(String arg0, String arg1) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getFigureBlock(java.lang.String, java.lang.String) */
-    @Override
-    protected String getFigureBlock(String arg0, String arg1) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getHeadBlock() */
-    @Override
-    protected String getHeadBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getHorizontalRuleBlock() */
-    @Override
-    protected String getHorizontalRuleBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getItalicBlock(java.lang.String) */
-    @Override
-    protected String getItalicBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getLineBreakBlock() */
-    @Override
-    protected String getLineBreakBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getLinkBlock(java.lang.String, java.lang.String) */
-    @Override
-    protected String getLinkBlock(String arg0, String arg1) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getListBlock(java.lang.String) */
-    @Override
-    protected String getListBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getMonospacedBlock(java.lang.String) */
-    @Override
-    protected String getMonospacedBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getNonBreakingSpaceBlock() */
-    @Override
-    protected String getNonBreakingSpaceBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getNumberedListBlock(java.lang.String) */
-    @Override
-    protected String getNumberedListBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getPageBreakBlock() */
-    @Override
-    protected String getPageBreakBlock() {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getParagraphBlock(java.lang.String) */
-    @Override
-    protected String getParagraphBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getRawTextBlock(java.lang.String) */
-    @Override
-    protected String getRawTextBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection1Block(java.lang.String) */
-    @Override
-    protected String getSection1Block(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection2Block(java.lang.String) */
-    @Override
-    protected String getSection2Block(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection3Block(java.lang.String) */
-    @Override
-    protected String getSection3Block(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection4Block(java.lang.String) */
-    @Override
-    protected String getSection4Block(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection5Block(java.lang.String) */
-    @Override
-    protected String getSection5Block(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSectionTitleBlock(java.lang.String) */
-    @Override
-    protected String getSectionTitleBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTableBlock(java.lang.String, java.lang.String) */
-    @Override
-    protected String getTableBlock(String arg0, String arg1) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTextBlock(java.lang.String) */
-    @Override
-    protected String getTextBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTitleBlock(java.lang.String) */
-    @Override
-    protected String getTitleBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getVerbatimBlock(java.lang.String) */
-    @Override
-    protected String getVerbatimBlock(String arg0) {
-        // TODO: Auto-generated method stub
-        return null;
-    }
-
-    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#isXmlSink() */
+    /** {@inheritDoc} */
     @Override
     protected boolean isXmlSink() {
-        // TODO: Auto-generated method stub
         return false;
     }
 
-    /** @see org.apache.maven.doxia.AbstractModuleTest#outputExtension() */
-    @Override
-    protected String outputExtension() {
-        // TODO: Auto-generated method stub
+    /** Not used.
+     * {@inheritDoc} */
+    protected String getTitleBlock( String title )
+    {
         return null;
     }
-     
+
+    /** Not used
+     * {@inheritDoc} */
+    @Override
+    protected String getAuthorBlock( String author) 
+    {
+        return null;
+    }
+
+    /** Not used
+     * {@inheritDoc} */
+    @Override
+    protected String getDateBlock( String date) 
+    {
+        return null;
+    }
+
+    /** Not used
+     * {@inheritDoc} */
+    @Override
+    protected String getHeadBlock() 
+    {
+        return null;
+    }
+
+    /** Not used 
+     * {@inheritDoc} */
+    @Override
+    protected String getBodyBlock() 
+    {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSectionTitleBlock( String title) {
+        return title;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSection1Block( String title)
+    {
+        return StringUtils.repeat( "#", 1 ) + title + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSection2Block( String title) 
+    {
+        return StringUtils.repeat( "#", 2 ) + title + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSection3Block( String title)
+    {
+        return StringUtils.repeat( "#", 3 ) + title + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSection4Block(String title)
+    {
+        return StringUtils.repeat( "#", 4 ) + title + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getSection5Block(String title)
+    {
+        return StringUtils.repeat( "#", 5 ) + title + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getListBlock(String item) {
+        return LIST_ITEM_MARKUP + item + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getNumberedListBlock(String item) {
+        return NUMBERING_LOWER_ROMAN_MARKUP + " " + item + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getDefinitionListBlock(String definum, String definition) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getFigureBlock(String source, String caption) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getTableBlock(String cell, String caption) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getParagraphBlock(String text) {
+        return text + EOL + EOL;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getVerbatimBlock(String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getHorizontalRuleBlock() {
+        return HORIZONTAL_RULE_MARKUP;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getPageBreakBlock() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getAnchorBlock(String anchor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getLinkBlock(String link, String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getItalicBlock(String text) {
+        return ITALIC_START_MARKUP + text + ITALIC_END_MARKUP;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getBoldBlock(String text) {
+        return BOLD_ITALIC_START_MARKUP + text + BOLD_ITALIC_END_MARKUP;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getMonospacedBlock(String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getLineBreakBlock() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getNonBreakingSpaceBlock() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getTextBlock(String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getRawTextBlock(String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String getCommentBlock(String text) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected String outputExtension() {
+        return "md";
+    }
+
+    // ----------------------------------------------------------------------
+    // Override unused tests
+    // ----------------------------------------------------------------------
+
+    /** Not used.
+     * {@inheritDoc} */
+    public void testAuthor()
+    {
+        // nop
+    }
+
+    /** Not used.
+     * {@inheritDoc} */
+    public void testDate()
+    {
+        // nop
+    }
+
+    /** Not used.
+     * {@inheritDoc} */
+    public void testHead()
+    {
+        // nop
+    }
+
+    /** Not used.
+     * {@inheritDoc} */
+    public void testBody()
+    {
+        // nop
+    }
+
+    /** Not used.
+     * {@inheritDoc} */
+    public void testTitle()
+    {
+        // nop
+    }
 }
