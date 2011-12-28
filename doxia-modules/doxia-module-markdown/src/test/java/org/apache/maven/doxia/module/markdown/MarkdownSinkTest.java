ce9449a5b4bf8d49b983fae22d10e90a16816f34
Cristian Pereyra <criis.pereyra@gmail.com>
2011-12-28 16:43:40 -0300

    base test added

--- doxia-modules/doxia-module-markdown/src/test/java/org/apache/maven/doxia/module/markdown/MarkdownSinkTest.java
+++ doxia-modules/doxia-module-markdown/src/test/java/org/apache/maven/doxia/module/markdown/MarkdownSinkTest.java
@@ -2,12 +2,15 @@ package org.apache.maven.doxia.module.markdown;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.io.Writer;
 
 import junit.framework.Assert;
 
 import org.apache.maven.doxia.logging.Log;
 import org.apache.maven.doxia.module.twiki.TWikiParser;
 import org.apache.maven.doxia.parser.ParseException;
+import org.apache.maven.doxia.sink.AbstractSinkTest;
+import org.apache.maven.doxia.sink.Sink;
 import org.junit.Test;
 
 import com.zaubersoftware.labs.twiki2markdown.MarkdownSink;
@@ -23,60 +26,237 @@ import com.zaubersoftware.labs.twiki2markdown.MarkdownSink;
  * @author Cristian Pereyra
  * @since Dec 26, 2011
  */
-public class MarkdownSinkTest {
-    
-    @Test
-    public void testParagraph() throws ParseException {
+public class MarkdownSinkTest extends AbstractSinkTest{
 
-    }
-    
-    @Test
-    public void testItalics() throws ParseException {
-    
-    }
-    
-    @Test
-    public void testBold() throws ParseException {
-    
-    }
-    
-    @Test
-    public void testCode() throws ParseException {
-
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#createSink(java.io.Writer) */
+    @Override
+    protected Sink createSink(Writer arg0) {
+        // TODO: Auto-generated method stub
+        return null;
     }
 
-    @Test
-    public void testHeadings() throws ParseException {
-
-    }
-    
-    @Test
-    public void testLinks() throws ParseException {
-
-    }
-    
-    @Test
-    public void testImages() throws ParseException {
-
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getAnchorBlock(java.lang.String) */
+    @Override
+    protected String getAnchorBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
     }
 
-    @Test
-    public void testOrderedLists() throws ParseException {
-
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getAuthorBlock(java.lang.String) */
+    @Override
+    protected String getAuthorBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
     }
 
-    @Test
-    public void testUnorderedLists() throws ParseException {
-
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getBodyBlock() */
+    @Override
+    protected String getBodyBlock() {
+        // TODO: Auto-generated method stub
+        return null;
     }
-    
-    @Test
-    public void testHorizontalRules() throws ParseException {
 
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getBoldBlock(java.lang.String) */
+    @Override
+    protected String getBoldBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
     }
-   
-    @Test
-    public void testEscaping() throws ParseException {
 
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getCommentBlock(java.lang.String) */
+    @Override
+    protected String getCommentBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
     }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getDateBlock(java.lang.String) */
+    @Override
+    protected String getDateBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getDefinitionListBlock(java.lang.String, java.lang.String) */
+    @Override
+    protected String getDefinitionListBlock(String arg0, String arg1) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getFigureBlock(java.lang.String, java.lang.String) */
+    @Override
+    protected String getFigureBlock(String arg0, String arg1) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getHeadBlock() */
+    @Override
+    protected String getHeadBlock() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getHorizontalRuleBlock() */
+    @Override
+    protected String getHorizontalRuleBlock() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getItalicBlock(java.lang.String) */
+    @Override
+    protected String getItalicBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getLineBreakBlock() */
+    @Override
+    protected String getLineBreakBlock() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getLinkBlock(java.lang.String, java.lang.String) */
+    @Override
+    protected String getLinkBlock(String arg0, String arg1) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getListBlock(java.lang.String) */
+    @Override
+    protected String getListBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getMonospacedBlock(java.lang.String) */
+    @Override
+    protected String getMonospacedBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getNonBreakingSpaceBlock() */
+    @Override
+    protected String getNonBreakingSpaceBlock() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getNumberedListBlock(java.lang.String) */
+    @Override
+    protected String getNumberedListBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getPageBreakBlock() */
+    @Override
+    protected String getPageBreakBlock() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getParagraphBlock(java.lang.String) */
+    @Override
+    protected String getParagraphBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getRawTextBlock(java.lang.String) */
+    @Override
+    protected String getRawTextBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection1Block(java.lang.String) */
+    @Override
+    protected String getSection1Block(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection2Block(java.lang.String) */
+    @Override
+    protected String getSection2Block(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection3Block(java.lang.String) */
+    @Override
+    protected String getSection3Block(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection4Block(java.lang.String) */
+    @Override
+    protected String getSection4Block(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSection5Block(java.lang.String) */
+    @Override
+    protected String getSection5Block(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getSectionTitleBlock(java.lang.String) */
+    @Override
+    protected String getSectionTitleBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTableBlock(java.lang.String, java.lang.String) */
+    @Override
+    protected String getTableBlock(String arg0, String arg1) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTextBlock(java.lang.String) */
+    @Override
+    protected String getTextBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getTitleBlock(java.lang.String) */
+    @Override
+    protected String getTitleBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#getVerbatimBlock(java.lang.String) */
+    @Override
+    protected String getVerbatimBlock(String arg0) {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+
+    /** @see org.apache.maven.doxia.sink.AbstractSinkTest#isXmlSink() */
+    @Override
+    protected boolean isXmlSink() {
+        // TODO: Auto-generated method stub
+        return false;
+    }
+
+    /** @see org.apache.maven.doxia.AbstractModuleTest#outputExtension() */
+    @Override
+    protected String outputExtension() {
+        // TODO: Auto-generated method stub
+        return null;
+    }
+     
 }
