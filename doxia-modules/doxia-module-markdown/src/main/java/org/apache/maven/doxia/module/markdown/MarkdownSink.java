4506e22e7fb7f7b48d65127bd6c7e35a80bbbffd
Cristian Pereyra <criis.pereyra@gmail.com>
2012-01-02 14:29:55 -0300

    markdown verbatim fix

--- doxia-modules/doxia-module-markdown/src/main/java/org/apache/maven/doxia/module/markdown/MarkdownSink.java
+++ doxia-modules/doxia-module-markdown/src/main/java/org/apache/maven/doxia/module/markdown/MarkdownSink.java
@@ -86,7 +86,7 @@ public class MarkdownSink extends AbstractTextSink {
                         outputWrite("  ");
                     }
                 }
-                outputWrite("     " + lines[i]);
+                outputWrite(MarkdownMarkup.VERBATIM_STYLE + lines[i]);
                 outputWrite("\n");    
             }
         }
